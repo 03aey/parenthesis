2e4f03ff176dca74bc376cb0c53915f82fd0ff1f
Alok Yadav <yalok6321@gmail.com>
2025-08-17 18:39:18 +0530

    authentication using next-auth and prisma

---
+++ db/prisma.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@/lib/generated/prisma";
+
+const prismaClientSingleton = () => {
+	return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+declare global {
+	var prisma: PrismaClientSingleton | undefined;
+}
+
+const prisma = global.prisma ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== "production") {
+	global.prisma = prisma;
+}
+
+export default prisma;
