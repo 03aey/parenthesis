ab645b14e3b3a2ab9a45353cf8371094068a9e94
Alok Yadav <yalok6321@gmail.com>
2026-06-29 15:08:17 +0530

    add proxy for protected route

--- proxy.ts
+++ proxy.ts
@@ -1,1 +1,54 @@
-export { auth as proxy } from "@/auth";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+const authRoutes = ["/sign-in", "/get-started"];
+
+const protectedRoutes = ["/profile", "/bookings", "/wishlist", "/settings"];
+
+export default auth((req) => {
+	const session = req.auth;
+
+	const pathname = req.nextUrl.pathname;
+
+	const isLoggedIn = !!session;
+
+	const isAuthRoute = authRoutes.includes(pathname);
+
+	const isProtectedRoute = protectedRoutes.some((route) =>
+		pathname.startsWith(route),
+	);
+
+	const callbackUrl = pathname + req.nextUrl.search;
+
+	/**
+	 * Guest-only routes
+	 */
+	if (isAuthRoute) {
+		if (isLoggedIn) {
+			return NextResponse.redirect(new URL("/", req.url));
+		}
+
+		return NextResponse.next();
+	}
+
+	/**
+	 * Protected routes
+	 */
+	if (isProtectedRoute && !isLoggedIn) {
+		return NextResponse.redirect(
+			new URL(
+				`/sign-in?callbackUrl=${encodeURIComponent(callbackUrl)}`,
+				req.url,
+			),
+		);
+	}
+
+	/**
+	 * Everything else is public
+	 */
+	return NextResponse.next();
+});
+
+export const config = {
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
